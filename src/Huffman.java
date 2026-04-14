4d7b9aea1531fb1d62f882b4b25e6976cb14d6d9
Robert Lewis <caysonlewis@gmail.com>
2026-04-14 17:07:16 -0500

    added genCode and printStats to Huffman

--- src/Huffman.java
+++ src/Huffman.java
@@ -16,35 +16,92 @@ public class Huffman
         }
     }
 
-    public static HuffNode buildHuffman()
+    public static void buildHuffman()
     {
-        ArrayList<HuffNode> Tree = new ArrayList<>();
+        ArrayList<HuffNode> heap = new ArrayList<>();
 
         for (char c : freqMap.keySet())
         {
             HuffNode node = new HuffNode(c, freqMap.get(c));
-            Tree.add(node);
+            heap.add(node);
         }
-        while (Tree.size() > 1)
+        while (heap.size() > 1)
         {
-            for (int i = 0; i < Tree.size() - 1; i++)
+            for (int i = 0; i < heap.size() - 1; i++)
             {
-                for (int j = 0; j < Tree.size() - i - 1; j++)
+                for (int j = 0; j < heap.size() - i - 1; j++)
                 {
-                    if (Tree.get(j).frequency > Tree.get(j+1).frequency)
+                    if (heap.get(j).frequency > heap.get(j+1).frequency)
                     {
-                        HuffNode temp = Tree.get(j);
-                        Tree.set(j, Tree.get(j + 1));
-                        Tree.set(j + 1, temp);
+                        HuffNode temp = heap.get(j);
+                        heap.set(j, heap.get(j + 1));
+                        heap.set(j + 1, temp);
                     }
                 }
             }
 
-            HuffNode left = Tree.remove(0);
-            HuffNode right = Tree.remove(0);
+            HuffNode left = heap.remove(0);
+            HuffNode right = heap.remove(0);
             HuffNode parent = new HuffNode(left, right);
-            Tree.add(parent);
+            heap.add(parent);
         }
-        return Tree.get(0);
+        root = heap.get(0);
+    }
+
+    public static void genCode()
+    {
+        codeMap = new HashMap<>();
+        generate(root, "");
+    }
+
+    public static void generate(HuffNode node, String code)
+    {
+        if (node == null)
+            return;
+
+        if (node.left == null && node.right == null)
+        {
+            codeMap.put(node.letter, code);
+            return;
+        }
+
+        generate(node.left, code + 0);
+        generate(node.right, code + 1);
+    }
+
+    public static String encode(String text)
+    {
+        String encoded = "";
+        for (int i = 0; i < text.length(); i++)
+        {
+            encoded += codeMap.get(text.charAt(i));
+        }
+
+        return encoded;
+    }
+
+    public static void printStats(String text)
+    {
+        System.out.println("Text(s): " + text);
+
+        System.out.println("Frequencies: ");
+        for (char c: freqMap.keySet())
+        {
+            if (c == ' ')
+                System.out.println("(space): " + freqMap.get(c));
+            else
+                System.out.println(c + ": " + freqMap.get(c));
+        }
+        System.out.println("Huffman Codes:");
+        for (char c : codeMap.keySet())
+        {
+            if (c == ' ')
+                System.out.println("(space): " + codeMap.get(c));
+            else
+                System.out.println(c + ": " + codeMap.get(c));
+        }
+
+        System.out.println("Encoded:");
+        System.out.println(encode(text));
     }
 }
